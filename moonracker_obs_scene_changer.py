95d8919f070d46864b739831b105ee5579fb8478
FlisherOfatale <37115003+FlisherOfatale@users.noreply.github.com>
2024-11-11 20:31:50 -0500

    added configuration file

--- moonracker_obs_scene_changer.py
+++ moonracker_obs_scene_changer.py
@@ -29,20 +29,16 @@ import json
 
 import sys
 import time
-
+import yaml
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
 sys.path.append('../')
 from obswebsocket import obsws, requests  # noqa: E402
-obs_password = "aaabbbccc"
-obs_host = "192.168.2.147"
-obs_port = 4455
-printer_ip = "192.168.2.115"
-printer_api_port = 7125
+with open('config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
 
-printer_ws_url = "ws://%s:%s/websocket?token=" % ( printer_ip, str(printer_api_port))
-
+printer_ws_url = "ws://%s:%s/websocket?token=" % ( config['printer']['host'], str(config['printer']['moonracker_port']))
 def on_close(printer_ws, close_status, close_msg):
   pass
 
@@ -50,44 +46,30 @@ def on_error(printer_ws, error):
   print("Websocket error: %s" % error)
 
 def setCamera(cameraType):
-  camera = {
-     "Toolchange" : "Toolchange",
-     "Printing" : "Printing",
-  }
-  obs_websocket.call(requests.SetCurrentProgramScene(sceneName=camera.get(cameraType)))
-              
+  obs_websocket.call(requests.SetCurrentProgramScene(sceneName=camera.get(config['obs']['scene'][cameraType])))
               
 def on_message(printer_ws, msg):
   response = json.loads(msg)
-  
-  # Subscribed printer objects are send with method: "notifiy_status_update"
-  # The subscribed objects are only published when the value has changed.
-  # e.g. bed_temperature target set to 50°, extruder temperature has changed, bed_temperature has changed, a.s.o.
-  #if response['method'] == "notify_status_update":
-  #  print(json.dumps(response, indent=2))
  
   if response['method'] == "notify_gcode_response":
     print(json.dumps(response, indent=3))  
     response_params_list = response.get("params")
     for response_log in response_params_list:
         if response_log.startswith("echo: Toolchange Starting"):
-            setCamera("Toolchange")
+            setCamera("ToolChanging")
         elif response_log.startswith("echo: Toolchange Completed"):
             setCamera("Printing")
   #else:
   # print(json.dumps(response, indent=3))
 
 def on_open(printer_ws):
-
   print("on_open()...")
-
   #Unsubscribe from any printer objects
   data = {
     "jsonrpc": "2.0",
     "method": "printer.objects.subscribe",
     "params": {
         "objects": {
-        
         }
     },
     "id": 4654
@@ -98,7 +80,7 @@ def on_open(printer_ws):
 printer_ws = websocket.WebSocketApp(url=printer_ws_url, on_close=on_close, on_error=on_error, on_message=on_message, on_open=on_open)
 
 ## OBS Websocket studd
-obs_websocket = obsws(obs_host, obs_port, obs_password)
+obs_websocket = obsws(config['obs']['host'], config['obs']['port'], config['obs']['password'])
 obs_websocket.connect()
 
 printer_ws.run_forever()
